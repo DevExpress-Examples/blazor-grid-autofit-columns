6b9f0cf1b91095999d3eacd5cdd0e5485b8c066d
iancaringal-devexpress <ian.caringal@devsoft.ph>
2025-12-04 16:50:26 +0800

    initial commit

---
+++ CS/DxGrid.AutoFit/Program.cs
@@ -0,0 +1,34 @@
+﻿using DxGrid.AutoFit.Components;
+using DxGrid.AutoFit.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDevExpressBlazor(options =>
+{
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+});
+builder.Services.AddMvc();
+builder.Services.AddScoped<DxThemesService>();
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AllowAnonymous();
+
+app.Run();
